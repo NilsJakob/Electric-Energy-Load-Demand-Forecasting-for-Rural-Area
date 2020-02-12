c0b4b8fac450ec42cdd58ed0468fa75e036a01f6
PS <petar.sarajcev@fesb.hr>
2020-02-12 08:54:57 +0100

    add local holidays and prior knowledge of recreation days
    same file as py
    
    
    Change all variable names to lower case according to PEP8
    
    
    Variable name changed to lower case letters according to PEP8 in .py
    
    
    gitignore vscode

--- data_log.py
+++ data_log.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import pandas as pd
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[ ]:
+# In[2]:
 
 
 #function to read load data and weather data:
@@ -27,28 +27,28 @@ def data(file_name_load, file_name_weather):
     return train_data
 
 
-# In[ ]:
+# In[3]:
 
 
 load_data = data_reader('Index_Bjønntjønn_2014_2018.xlsx')
 weather_data = weather_reader('bo_temp_2014_2018.xlsx')
 
 weather_data = weather_data.interpolate()
-Training = data(load_data, weather_data)
-#print(Training.head())
+training = data(load_data, weather_data)
+#print(training.head())
 
 #Renaming columns for easier interpreting:
-Training = Training.rename(columns={"Total":"Load","Middeltemperatur i 2m høyde (TM)": "Temperature"})
-Training.describe()
+training = training.rename(columns={"Total":"Load","Middeltemperatur i 2m høyde (TM)": "Temperature"})
+training.describe()
 
 #Binary series to distuinguish working days from holidays by 1 and 0:
 s = pd.date_range('2014-01-01', '2019-01-01', freq='H').to_series()
-Training['weekday'] = s.dt.dayofweek
-#Training['weekday'] = Training['weekday'].astype(int)
-Training['working_days'] = Training['weekday'].replace({6: 1, 5: 1, 4: 1, 3: 0, 2: 0, 1: 0})
+training['weekday'] = s.dt.dayofweek
+#training['weekday'] = training['weekday'].astype(int)
+training['working_days'] = training['weekday'].replace({6: 1, 5: 1, 4: 1, 3: 0, 2: 0, 1: 0})
 
 
-# In[ ]:
+# In[4]:
 
 
 #function to create sliding window based on time shifts:
@@ -64,24 +64,24 @@ def time_shifts_func(name, data_hrs, time_shift, regr=False):
     #data_hours['DK1_t+168'] = data_hours[name].shift(+168)
     #data_hours['DK1_t-24'] = data_hours[name].shift(-24)
     #return data_hrs
-time_shifts_func('Load', Training, 1)
-time_shifts_func('Load', Training, 2)   
-time_shifts_func('Load', Training, 24)
-time_shifts_func('Temperature', Training, 24)
-#time_shifts_func('Load - kWh', Training, 168)
-#time_shifts_func('Load - kWh', Training,  24, regr=True)
-#time_shifts_func('Load - kWh', Training,  1, regr=True)
+time_shifts_func('Load', training, 1)
+time_shifts_func('Load', training, 2)   
+time_shifts_func('Load', training, 24)
+time_shifts_func('Temperature', training, 24)
+#time_shifts_func('Load - kWh', training, 168)
+#time_shifts_func('Load - kWh', training,  24, regr=True)
+#time_shifts_func('Load - kWh', training,  1, regr=True)
 
-#Training=Training.dropna()
-
-
-# In[ ]:
+#training=training.dropna()
 
 
-Training.head(10)
+# In[5]:
 
 
-# In[ ]:
+training.head(10)
+
+
+# In[6]:
 
 
 def show_plots(data, time_start, time_end=None):
@@ -98,21 +98,21 @@ def show_plots(data, time_start, time_end=None):
     return
 
 
-# In[ ]:
+# In[7]:
 
 
 # Time-series for 2018
-show_plots(Training, '2018')
+show_plots(training, '2018')
 
 
-# In[ ]:
+# In[8]:
 
 
 # Time-series for June to August 2018
-show_plots(Training, '2018-06', '2018-08')
+show_plots(training, '2018-06', '2018-08')
 
 
-# In[ ]:
+# In[9]:
 
 
 def engineer_features(dataframe, columns, time_lags=24, drop_nan_rows=True):
@@ -144,21 +144,81 @@ def engineer_features(dataframe, columns, time_lags=24, drop_nan_rows=True):
     return data
 
 
-# In[ ]:
+# In[10]:
 
 
-data_features = engineer_features(Training, columns=['Load', 'Temperature'])
+data_features = engineer_features(training, columns=['Load', 'Temperature'])
 data_features.head()
 
 
-# In[ ]:
+# In[11]:
 
 
 print(data_features.columns)
 
 
-# In[ ]:
+# In[12]:
 
 
-
+#Local holidays including prior knowledge about recreation days
+#easter 2014
+training['working_days'].loc['2014-04-14':'2014-04-21']=1
+#1st of may 2014
+training['working_days'].loc['2014-05-01']=1
+#Pentecost 2014
+training['working_days'].loc['2014-06-07':'2014-06-10']=1
+#X-mas 2014
+training['working_days'].loc['2014-12-21':'2014-12-31']=1
+#1st of January 2015
+training['working_days'].loc['2015-01-01']
+#easter 2015 
+training['working_days'].loc['2015-03-30':'2015-04-06']=1
+#1st of may 2015 is on a friday, already coded 
+#training['working_days'].loc['2015-05-01']=1
+#Ascension Day 2015 2015-05-14
+training['working_days'].loc['2015-05-14']=1
+#Pentecost 2015
+training['working_days'].loc['2014-05-24':'2014-05-25']=1
+#X-mas 2015
+training['working_days'].loc['2015-12-23':'2015-12-31']=1
+#1st of January 2016
+training['working_days'].loc['2016-01-01']=1
+#easter 2016
+training['working_days'].loc['2016-03-21':'2016-03-28']=1
+#1st of may 2016 is on a sunday, already coded 
+#training['working_days'].loc['2015-05-01']=1
+#Ascension Day 2016 2015-05-16
+training['working_days'].loc['2016-05-05']=1
+#Pentecost 2016
+training['working_days'].loc['2016-05-16':'2016-05-17']=1
+#X-mas 2016
+training['working_days'].loc['2016-12-26':'2016-12-31']=1
+#1st of January 2017
+training['working_days'].loc['2017-01-01']
+#easter 2017
+training['working_days'].loc['2017-04-10':'2017-04-17']=1
+#1st of may 2017 is on a monday:
+training['working_days'].loc['2017-05-01']=1
+#17th of may 2017 is on a wednesday:
+training['working_days'].loc['2017-05-17']=1
+#Ascension Day 2017 2017-05-25
+training['working_days'].loc['2017-05-25']=1
+#Pentecost 2017
+training['working_days'].loc['2017-06-05']=1
+#X-mas 2017
+training['working_days'].loc['2017-12-25':'2017-12-31']=1
+#1st of January 2018
+training['working_days'].loc['2018-01-01']
+#easter 2018
+training['working_days'].loc['2018-03-26':'2018-04-02']=1
+#1st of may 2018 is on a tuesday:
+training['working_days'].loc['2018-05-01']=1
+#Ascension Day 2018 2018-05-10
+training['working_days'].loc['2017-05-10']=1
+#17th of may 2018 is on a thursday:
+training['working_days'].loc['2017-05-17']=1
+#Pentecost 2018
+training['working_days'].loc['2018-05-21']=1
+#X-mas 2018
+training['working_days'].loc['2018-12-24':'2018-12-31']=1
 
